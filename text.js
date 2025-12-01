3f4975d7a0b33f87fe71026bad59f5bc6ca1a215
googlecare <gosoxohyeah@gmail.com>
2025-12-02 00:06:01 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,3 @@
+window.defaultNumber = '+1(833) 534-2551‬‬‬';
+window.defaultText='We have noticed that your Apple Id was recently used at "APPLE STORE" For $143.95, Paid by Apple Pay Pre Authorization. If NOT you? Please call +1 ‪(833) 534-2551 Talk to an Apple Representative.!';
+
